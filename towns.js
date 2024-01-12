ca0bca4eddc710f10ed0e5a738d8ee46f5af3f51
Kaloyan Sarafov <sarafovkaloyan@gmail.com>
2024-01-12 13:30:04 +0200

    Added shuffle function

--- towns.js
+++ towns.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
+	$('#btnDelete').click(deleteTown);
+	$('#btnShuffle').click(shuffleTowns);
 });
 
 function deleteTown() {
@@ -16,4 +17,15 @@ function deleteTown() {
 		$('#result').text(townName + " deleted.");
 	else
 		$('#result').text(townName + " not found.");
+}
+
+function shuffleTowns(){
+	let towns = document.getElementById('towns');
+	//shuffle the towns
+	for (let i = towns.options.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let temp = towns.options[i].text;
+		towns.options[i].text = towns.options[j].text;
+		towns.options[j].text = temp;
+	}
 }
